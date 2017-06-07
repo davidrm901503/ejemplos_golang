2c51f0e18a71744f891af0b61062ac76fd80ef9c
David <davidrm901503@gmail.com>
2017-06-07 12:46:55 -0500

    agregando detalles a tcpClient

--- server.go
+++ server.go
@@ -3,24 +3,42 @@ package main
 import "net"
 import "fmt"
 import "bufio"
-import "strings"
+import (
+  "strings"
+  "os"
+  "./config/connection"
+)
 
 
 func handleConnectionServer(conn net.Conn) {
-
+  for {
   msg, _ := bufio.NewReader(conn).ReadString('\n')
   if  len( msg)  > 0   {
     fmt.Print("income msg: " + msg)
     enviar := strings.ToUpper(msg)
     conn.Write([]byte(enviar + "\n"))
+    //conn.Close()
+   }
   }
 }
 func main() {
 
 	fmt.Println("levantando servidor...")
-	ln, _ := net.Listen("tcp", ":8081")
-	conn, _ := ln.Accept()
+	ln, err := net.Listen("tcp", ":8081")
+  if err != nil {
+    fmt.Println("Error levantando el server:", err.Error())
+    os.Exit(1)
+  }
+  defer ln.Close()
+  conn_settings :=connection.LoadSettings()
+  fmt.Println("Escuchando por: " + conn_settings.Host + ":" + conn_settings.Port + "("+conn_settings.Protocol+")")
+
 	for {
-    handleConnectionServer(conn)
+    conn, err := ln.Accept()
+    if err != nil {
+      fmt.Println("Error acceptando peticiones de clientes: ", err)
+      os.Exit(1)
+    }
+    go handleConnectionServer(conn)
 	}
 }
